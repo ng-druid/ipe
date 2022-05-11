f70b646c920f38c3df3a39fd760e4336f9dc7926
druid <angular.druid@gmail.com>
2022-05-11 05:23:38 -0400

    Update druid to 0.0.36 and implement rum.

--- projects/ipe/src/app/app.module.ts
+++ projects/ipe/src/app/app.module.ts
@@ -69,6 +69,7 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
 import { RefineryModule } from '@ng-druid/refinery';
 import { SheathModule } from '@ng-druid/sheath';
 // import { PanelpageModule } from 'panelpage';
+import { CloudwatchRumSettings, CLOUDWATCH_RUM_SETTINGS, initializeRumMonitorFactory } from '@ng-druid/awrum';
 
 // import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 // import { MonacoEditorModule } from 'ngx-monaco-editor';
@@ -220,6 +221,7 @@ export function markedOptionsFactory(): MarkedOptions {
     { provide: ALIENALIAS_SETTINGS, useValue: new AlienaliasSettings(environment.alienaliasSettings) },
 
     { provide: COGNITO_SETTINGS, useValue: new CognitoSettings(environment.cognitoSettings) },
+    { provide: CLOUDWATCH_RUM_SETTINGS, useValue: new CloudwatchRumSettings(environment.rumSettings) },
     // { provide: LOGGING_SETTINGS, useValue: new LoggingSettings(environment.loggingSettings) },
     // { provide: AD_SETTINGS, useValue: new AdSettings(environment.adSettings) },
     // { provide: TAXONOMY_SETTINGS, useValue: new TaxonomySettings(environment.taxonomySettings) },
@@ -233,6 +235,8 @@ export function markedOptionsFactory(): MarkedOptions {
     { provide: HTTP_INTERCEPTORS, useClass: LogoutInterceptor, multi: true },
 
     { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
+
+    { provide: APP_INITIALIZER, useFactory: initializeRumMonitorFactory, multi: true, deps: [ CLOUDWATCH_RUM_SETTINGS ] },
 
         /* These are required only for pre-rendering - quick hack to make work for now */
     //{ provide: APP_BASE_HREF, useValue: 'http://localhost:4000/' },

7994a721cf0e76055d57f7532abbb1988ded7438
druid <angular.druid@gmail.com>
2022-07-07 16:38:55 -0400

    Test json objects read workflow inside druid.

--- projects/spear/src/environments/environment.ts
+++ projects/spear/src/environments/environment.ts
@@ -116,7 +116,7 @@ export const environment = {
   }
 };
 
-const x = 'xxxxxxxxxxxxxxsxxx';
+const x = 'xxxxxxxxxxxxxxxsxxx';
 
 /*
  * For easier debugging in development mode, you can import the following file
